82242f43c40491cff27eeb3793f10e8e290c23c1
shimozuki <robbishimozuki@gmail.com>
2022-12-16 08:09:50 +0800

    master

--- lib/screens/sajadah_screen.dart
+++ lib/screens/sajadah_screen.dart
@@ -8,26 +8,28 @@ import 'package:myquran_app/screens/sajadah_ayah.dart';
 class SajadahScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Color(0xFFE8FCE8),
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new_rounded), onPressed: () {
-          Navigator.push(
-            context,
-            PageRouteBuilder(
-              pageBuilder: (c, a1, a2) => HomeScreen(),
-              transitionsBuilder: (c, anim, a2, child) => FadeTransition(opacity: anim, child: child),
-              transitionDuration: Duration(milliseconds: 500),
-            ),
-          );
-        },
+          icon: Icon(Icons.arrow_back_ios_new_rounded),
+          onPressed: () {
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (c, a1, a2) => HomeScreen(),
+                transitionsBuilder: (c, anim, a2, child) =>
+                    FadeTransition(opacity: anim, child: child),
+                transitionDuration: Duration(milliseconds: 500),
+              ),
+            );
+          },
         ),
         backgroundColor: Colors.transparent,
         elevation: 0.0,
         toolbarHeight: 60,
-        title: Text("List Sajadah",
+        title: Text(
+          "List Sajadah",
           style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Color(0xFFDEF1DE),
@@ -37,10 +39,11 @@ class SajadahScreen extends StatelessWidget {
         centerTitle: true,
         flexibleSpace: Container(
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
-              color: Color(0xFF5C956B),
-            )
-        ),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(15),
+              bottomRight: Radius.circular(15)),
+          color: Color(0xFF5C956B),
+        )),
       ),
       body: SafeArea(
         child: Stack(
@@ -54,14 +57,13 @@ class SajadahScreen extends StatelessWidget {
                   );
                 } else if (snapshot.hasData == null) {
                   return Center(
-                      child: Text(
-                          "Koneksi Error! Periksa internet anda"));
+                      child: Text("Koneksi Error! Periksa internet anda"));
                 } else if (snapshot.hasError) {
                   return Center(
                       child: Text(
-                        "Ada yang tak beres darimu!\nMenghubungkan kembali :)",
-                        textAlign: TextAlign.center,
-                      ));
+                    "Ada yang tak beres darimu!\nMenghubungkan kembali :)",
+                    textAlign: TextAlign.center,
+                  ));
                 } else if (snapshot.connectionState == ConnectionState.done) {
                   return Container(
                     color: Colors.transparent,
@@ -80,30 +82,30 @@ class SajadahScreen extends StatelessWidget {
                             onLongPress: () {
                               showDialog(
                                   builder: (context) => SajdaInformation(
-                                    juzNumber: snapshot
-                                        .data.sajdaAyahs[index].juzNumber,
-                                    rukuNumber: snapshot
-                                        .data.sajdaAyahs[index].rukuNumber,
-                                    sajdaNumber: snapshot
-                                        .data.sajdaAyahs[index].sajdaNumber,
-                                    surahName: snapshot
-                                        .data.sajdaAyahs[index].surahName,
-                                    surahEnglishName: snapshot.data
-                                        .sajdaAyahs[index].surahEnglishName,
-                                    englishNameTranslation: snapshot
-                                        .data
-                                        .sajdaAyahs[index]
-                                        .englishNameTranslation,
-                                    revelationType: snapshot
-                                        .data.sajdaAyahs[index].revelationType,
-                                  ), context: context);
+                                        juzNumber: snapshot
+                                            .data.sajdaAyahs[index].juzNumber,
+                                        rukuNumber: snapshot
+                                            .data.sajdaAyahs[index].rukuNumber,
+                                        sajdaNumber: snapshot
+                                            .data.sajdaAyahs[index].sajdaNumber,
+                                        surahName: snapshot
+                                            .data.sajdaAyahs[index].surahName,
+                                        surahEnglishName: snapshot.data
+                                            .sajdaAyahs[index].surahEnglishName,
+                                        englishNameTranslation: snapshot
+                                            .data
+                                            .sajdaAyahs[index]
+                                            .englishNameTranslation,
+                                        revelationType: snapshot.data
+                                            .sajdaAyahs[index].revelationType,
+                                      ),
+                                  context: context);
                             },
                             leading: Container(
                               padding: EdgeInsets.all(15),
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15),
-                                color: Color(0xFF4A7D58)
-                              ),
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Color(0xFF4A7D58)),
                               child: Text(
                                 "${snapshot.data.sajdaAyahs[index].sajdaNumber}",
                                 style: TextStyle(
@@ -114,13 +116,11 @@ class SajadahScreen extends StatelessWidget {
                               ),
                             ),
                             title: Text(
-                              "${snapshot.data.sajdaAyahs[index].surahEnglishName}",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25,
-                                  color: Color(0xFF5C956B)
-                              )
-                            ),
+                                "${snapshot.data.sajdaAyahs[index].surahEnglishName}",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25,
+                                    color: Color(0xFF5C956B))),
                             subtitle: Text(
                               "${snapshot.data.sajdaAyahs[index].englishNameTranslation}",
                             ),
@@ -133,31 +133,31 @@ class SajadahScreen extends StatelessWidget {
                                   context,
                                   MaterialPageRoute(
                                       builder: (context) => SajdaAyah(
-                                        surahName: snapshot.data
-                                            .sajdaAyahs[index].surahName,
-                                        surahEnglishName: snapshot
-                                            .data
-                                            .sajdaAyahs[index]
-                                            .surahEnglishName,
-                                        englishNameTranslation: snapshot
-                                            .data
-                                            .sajdaAyahs[index]
-                                            .englishNameTranslation,
-                                        juz: snapshot.data.sajdaAyahs[index]
-                                            .juzNumber,
-                                        manzil: snapshot.data
-                                            .sajdaAyahs[index].manzilNumber,
-                                        ruku: snapshot.data
-                                            .sajdaAyahs[index].rukuNumber,
-                                        sajdaAyahs: snapshot
-                                            .data.sajdaAyahs[index].text,
-                                        sajdaNumber: snapshot.data
-                                            .sajdaAyahs[index].sajdaNumber,
-                                        revelationType: snapshot
-                                            .data
-                                            .sajdaAyahs[index]
-                                            .revelationType,
-                                      )));
+                                            surahName: snapshot.data
+                                                .sajdaAyahs[index].surahName,
+                                            surahEnglishName: snapshot
+                                                .data
+                                                .sajdaAyahs[index]
+                                                .surahEnglishName,
+                                            englishNameTranslation: snapshot
+                                                .data
+                                                .sajdaAyahs[index]
+                                                .englishNameTranslation,
+                                            juz: snapshot.data.sajdaAyahs[index]
+                                                .juzNumber,
+                                            manzil: snapshot.data
+                                                .sajdaAyahs[index].manzilNumber,
+                                            ruku: snapshot.data
+                                                .sajdaAyahs[index].rukuNumber,
+                                            sajdaAyahs: snapshot
+                                                .data.sajdaAyahs[index].text,
+                                            sajdaNumber: snapshot.data
+                                                .sajdaAyahs[index].sajdaNumber,
+                                            revelationType: snapshot
+                                                .data
+                                                .sajdaAyahs[index]
+                                                .revelationType,
+                                          )));
                             },
                           ),
                         );
@@ -165,9 +165,7 @@ class SajadahScreen extends StatelessWidget {
                     ),
                   );
                 } else {
-                  return Center(
-                      child:
-                      Text("Koneksi Error, Mohon Coba lagi!"));
+                  return Center(child: Text("Koneksi Error, Mohon Coba lagi!"));
                 }
               },
             ),
@@ -189,12 +187,12 @@ class SajdaInformation extends StatefulWidget {
 
   SajdaInformation(
       {required this.surahName,
-        required this.sajdaNumber,
-        required this.juzNumber,
-        required this.rukuNumber,
-        required this.surahEnglishName,
-        required this.englishNameTranslation,
-        required this.revelationType});
+      required this.sajdaNumber,
+      required this.juzNumber,
+      required this.rukuNumber,
+      required this.surahEnglishName,
+      required this.englishNameTranslation,
+      required this.revelationType});
 
   @override
   _SajdaInformationState createState() => _SajdaInformationState();
@@ -235,68 +233,75 @@ class _SajdaInformationState extends State<SajdaInformation>
             width: 250,
             height: 250,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
-                color: Color(0xFF5C956B)
-            ),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15)),
+                color: Color(0xFF5C956B)),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Text(
-                  "Sajda Information",
-                  style: TextStyle( fontWeight: FontWeight.bold, color: Color(0xFFE8FCE8), fontSize: 25)
-                ),
+                Text("Sajda Information",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFE8FCE8),
+                        fontSize: 25)),
                 SizedBox(
                   height: height * 0.02,
                 ),
-
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-
                     Container(
                       padding: EdgeInsets.only(left: 25),
                       child: Text(
                         widget.surahEnglishName,
                         softWrap: false,
                         style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25,
-                          color: Color(0xFFE8FCE8)
-                        ),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Color(0xFFE8FCE8)),
                       ),
                     ),
-                    SizedBox( width: 25,),
-
+                    SizedBox(
+                      width: 25,
+                    ),
                     Expanded(
                       child: Container(
-                        child: Text(
-                          widget.surahName,
-                          softWrap: false,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Color(0xFFE8FCE8)
-                          )
-                        ),
+                        child: Text(widget.surahName,
+                            softWrap: false,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Color(0xFFE8FCE8))),
                       ),
                     ),
                   ],
                 ),
-                Text("Sajda Number: ${widget.sajdaNumber}", style: TextStyle(color: Color(0xFFE8FCE8))),
-                Text("Juz Number: ${widget.juzNumber}", style: TextStyle(color: Color(0xFFE8FCE8))),
-                Text("Ruku Number: ${widget.rukuNumber}", style: TextStyle(color: Color(0xFFE8FCE8))),
-                Text("Meaning: ${widget.englishNameTranslation}", style: TextStyle(color: Color(0xFFE8FCE8))),
-                Text("Chapter Type: ${widget.revelationType}", style: TextStyle(color: Color(0xFFE8FCE8))),
+                Text("Sajda Number: ${widget.sajdaNumber}",
+                    style: TextStyle(color: Color(0xFFE8FCE8))),
+                Text("Juz Number: ${widget.juzNumber}",
+                    style: TextStyle(color: Color(0xFFE8FCE8))),
+                Text("Ruku Number: ${widget.rukuNumber}",
+                    style: TextStyle(color: Color(0xFFE8FCE8))),
+                Text("Meaning: ${widget.englishNameTranslation}",
+                    style: TextStyle(color: Color(0xFFE8FCE8))),
+                Text("Chapter Type: ${widget.revelationType}",
+                    style: TextStyle(color: Color(0xFFE8FCE8))),
                 SizedBox(
                   height: height * 0.010,
                 ),
                 SizedBox(
                   height: height * 0.05,
-                  child: FlatButton(
-                        color: Color(0xFF4A7D58),
+                  child: TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: Color(0xFF4A7D58),
+                      ),
                       onPressed: () => Navigator.pop(context),
-                      child: Text("OK", style: TextStyle(color: Color(0xFFE8FCE8)),)),
+                      child: Text(
+                        "OK",
+                        style: TextStyle(color: Color(0xFFE8FCE8)),
+                      )),
                 )
               ],
             ),
